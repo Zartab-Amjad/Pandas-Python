5a032140bc0df39da32fff319d605b5702642a12
Zartab-Amjad <zartab10k@gmail.com>
2026-02-07 01:01:41 +0500

    Access specific value of row

--- first.py
+++ first.py
@@ -10,5 +10,12 @@ df = pd.DataFrame(data)
 print("Show records:\n", df) 
 
 #Change index..............
-df = pd.DataFrame(data, index = ["Row1", "Row2", "Row3"])
-print(f"DataFrame: \n {df}") 
+df = pd.DataFrame(data, index = ["Row1", "Row2", "Row3", "Row4", "Row5"]) 
+print(f"\nDataFrame after changing index: \n {df}") 
+
+#Access rows or cols.........
+print("\nAccessing row2:\n",df.loc["Row2"])  
+
+#Acess specific values of key............
+print("\nName of first row:", df.loc["Row1", "Name"])
+print("Marks of first row:", df.loc["Row1", "Marks"]) 
