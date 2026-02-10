5a2801d332f116e6169c326cde6cf1a709b74053
Zartab-Amjad <zartab10k@gmail.com>
2026-02-10 18:37:59 +0500

    methods updated by join

--- functions.py
+++ functions.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
-data = {
+data1 = {
     "Name" : ["Ali", "Sara", "Zara", "Raza", "Ahad", "Fahad", "Minahil", "Fatima"],
-    "Marks" : [89, 76, 93, 72, 81, 81, 95, 98]
+    "Rollno." : [101, 102, 103, 104, 105, 106, 107, 108],
 }
 
+data2 = {
+    "Marks" : [89, 76, 93, 72, 84, 82, 95, 98],
+    "Grade" : ["A", "B", "A+", "B", "A","A", "A+", "A+"],
+}
 
-df = pd.DataFrame(data)
+df = pd.DataFrame(data1)
+df2 = pd.DataFrame(data2)
 
 #..................Pandas built-in attributes.......................
 # 1. dtypes, to find data type...
@@ -24,7 +29,7 @@ print("\nShape of df:", df.shape)
 # 5. index, return index...
 print("\nIndex of df: ", df.index) 
 
-df1 = pd.DataFrame(data, index = ["Row1", "Row2", "Row3", "Row4", "Row5", "Row6", "Row7", "Row8"]) 
+df1 = pd.DataFrame(data1, index = ["Row1", "Row2", "Row3", "Row4", "Row5", "Row6", "Row7", "Row8"]) 
 print("\nAfter changing the Index of df: ", df1.index) 
 
 # 6. T, transpose of df...
@@ -40,4 +45,8 @@ print("\nReturn first 3 rows:\n ", df.head(3))
 print("\nTail of df:\n", df.tail())
 
 print("\nReturn last 2 rows:\n", df.tail(2))  
+
+# 3. join(), used to join two dataframes...
+joindf = df.join(df2)
+print("\nJoining two dataframes:\n", joindf)
 
